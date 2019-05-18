7e6410cd9c6a0c302b7835443fbed9cf7abbe6f1
shojaeix <shojaei.x@gmail.com>
2019-05-18 13:09:47 +0430

    add demo of convert api methods

---
+++ app/Http/Controllers/ConvertApiController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\ConvertorEx;
+use Illuminate\Http\Request;
+use Olifolkerd\Convertor\Convertor;
+use Olifolkerd\Convertor\Exceptions\ConvertorDifferentTypeException;
+use Olifolkerd\Convertor\Exceptions\ConvertorInvalidUnitException;
+
+class ConvertApiController extends Controller
+{
+    protected $quantities = [
+        'weight' => [
+            'title' => "وزن",
+            'units' => [
+                'Ton' => 'تن',
+                'kg' => 'کیلوگرم',
+            ]
+        ],
+        'force' => [
+            'title' => "نیرو",
+            'units' => [
+                'N' => 'نیوتن',
+                'gf' => 'گرم-نیرو',
+            ]
+        ],
+    ];
+
+    function getQuantities()
+    {
+        $quantities = [];
+        $convertor = new ConvertorEx;
+        $units = $convertor->getAllUnits();
+        foreach ($units as $key=>$values){
+            if(!isset($quantities[$values['base']])) $quantities[$values['base']] = [];
+
+            $quantities[$values['base']][$key] = $values;
+        }
+
+        return $quantities;
+    }
+
+    function getCompatibleUnits($unit){
+
+        foreach ($this->quantities as $values){
+            if(isset($values['units']) and in_array($unit, array_keys($values['units'])))
+                return $values['units'];
+        }
+        return [];
+    }
+
+    function convert(Request $request)
+    {
+        // validate inputs
+        $parameters = request()->validate([
+            'quantity' => 'required',
+            'first_unit' => 'required',
+            'second_unit' => 'required',
+            'value' => 'required|numeric',
+        ]);
+        $successful = false;
+        // convert values
+        try {
+            $converter = new Convertor($parameters['value'], $parameters['first_unit']);
+            $convertedValue = $converter->to($parameters['second_unit']);
+            $successful = true;
+        } catch(ConvertorInvalidUnitException $exception){
+            $errorDescription = 'واحد های وارد شده نامعتبر هستند.';
+        } catch(ConvertorDifferentTypeException $exception){
+            $errorDescription = 'واحد های وارد شده از کمیت های مختلف هستند.';
+        }
+        // create result array
+        $result = [
+            'successful' => $successful,
+        ];
+        if($successful) {
+            $result['result'] = $convertedValue;
+            $result['formula'] = 'دودوتا چهار تا';
+        } else {
+            $result['error_description'] = $errorDescription;
+        }
+        // array will encode automatically
+        return $result;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::any('getQuantities', 'ConvertApiController@getQuantities');
+
+Route::any('convert', 'ConvertApiController@convert');
+
+Route::any('getCompatibleUnits/{unit}', 'ConvertApiController@getCompatibleUnits');
