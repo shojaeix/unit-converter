15977516fd34fa115ba247566d0d7f8c1474b29a
Amin shojaei <shojaei.x@gmail.com>
2019-06-01 08:50:26 +0430

    add simple layout for views

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Amin
+ * Date: 5/18/2019
+ * Time: 10:30
+ */
+?>
+<html>
+<head>
+    <link rel="stylesheet" href="{{ url('css/app.css/') }}">
+</head>
+<body>@yield('content')</body>
+</html>
